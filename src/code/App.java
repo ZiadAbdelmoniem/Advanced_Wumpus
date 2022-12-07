621ff14c515f2eb0c47b2e2551d29d4e110ce8e3
ZiadAbdelmoniem <ziadearth@gmail.com>
2022-12-07 20:43:34 +0200

    Revert "aly Hatem"
    This reverts commit ee23abc916677a1fd61b0ea1cc532313d0683b4f.

--- src/code/App.java
+++
@@ -1,8 +0,0 @@
-package code;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println(CoastGuard.solve("7,5;40;2,3;3,6;1,1,10,4,5,90;","BF", false));
-    }
-    
-}
